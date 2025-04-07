5b3c270fefb8ecac4090f1166a6babfeb730a7bf
hellomsmahadi <webmsmahadi@gmail.com>
2025-04-08 03:42:30 +0600

    Update final 1 swiper-progress-slider.js

--- swiper-progress-slider.js
+++ swiper-progress-slider.js
@@ -1,101 +1,24 @@
-$(".swiper").append(`<div class="swiper-scrollbar"></div>`);
-$(".swiper").append(`<div class="swiper-pagination"></div>`);
-$(".swiper").append(`<div class="swiper-arrow button-prev"></div>`);
-$(".swiper").append(`<div class="swiper-arrow button-next"></div>`);
+$(".is-progress-slider").append(`<div class="swiper-scrollbar"></div>`);
+$(".is-progress-slider").append(`<div class="swiper-pagination"></div>`);
+$(".is-progress-slider").append(`<div class="swiper-arrow button-prev"></div>`);
+$(".is-progress-slider").append(`<div class="swiper-arrow button-next"></div>`);
 
-const swiper1 = new Swiper(".swiper1", {
-  // Optional parameters
+const progressSlider = new Swiper(".is-progress-slider", {
   direction: "horizontal",
-  loop: true,
-  slidesPerView: 1,
-  slidesPerGroup: 1,
-  spaceBetween: 20,
+  spaceBetween: 1,
+  slidesPerView: "auto",
+  grabCursor: true,
   loop: false,
-  centeredSlides: false,
   mousewheel: {
-    forceToAxis: true
+    forceToAxis: true,
   },
   speed: 300,
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 1
-    },
-    // when window width is >= 768px
-    768: {
-      slidesPerView: 2
-    },
-    // when window width is >= 992px
-    992: {
-      slidesPerView: 3
-    }
-  },
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true
-  },
-
-  // Navigation arrows
   navigation: {
     nextEl: ".button-next",
-    prevEl: ".button-prev"
+    prevEl: ".button-prev",
   },
-
-  // And if we need scrollbar
   scrollbar: {
     el: ".swiper-scrollbar",
-    draggable: true
-  }
-});
-
-const swiper2 = new Swiper(".swiper2", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-  slidesPerView: 1,
-  slidesPerGroup: 1,
-  spaceBetween: 20,
-  loop: false,
-  parallax: true,
-  centeredSlides: false,
-  mousewheel: {
-    forceToAxis: true
+    draggable: true,
   },
-  speed: 300,
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 1
-    },
-    // when window width is >= 768px
-    768: {
-      slidesPerView: 2
-    },
-    // when window width is >= 992px
-    992: {
-      slidesPerView: 2
-    }
-  },
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".button-next",
-    prevEl: ".button-prev"
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true
-  }
 });
